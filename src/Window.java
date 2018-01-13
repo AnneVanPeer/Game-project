97132dca77fe0da320dec653fe841394b620af4d
Anne <Anne@DESKTOP-OB5P1V7>
2018-01-13 16:30:59 +0100

    Windowtjuh

--- src/Window.java
+++ src/Window.java
@@ -1,18 +1,30 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Window{
 	
+	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	
 	public Window(Game game) {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
 		JFrame frame = new JFrame("map");
-		frame.setSize(1000, 2000);
-		this.fillFrame();
+		
+		this.fillFrame(frame);
 		frame.pack();
+		frame.setSize(screenSize);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+				
 	}
 
-	private void fillFrame() {
-		//test
-		
+	private void fillFrame(JFrame frame) {
+		frame.setBackground(Color.WHITE);
+		frame.add(new JLabel("hoii"));
 	}
 
 }
