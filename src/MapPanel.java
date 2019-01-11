06e63fa1de8f148a99d90cd4fe1a22545bfb54c6
avpeer <avpeer@avpeer.quintor.local>
2019-01-11 16:31:21 +0100

    nodes weergeven, maar het werkt niet :(

--- src/MapPanel.java
+++ src/MapPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -17,19 +18,24 @@ public class MapPanel extends JPanel{
 	
 	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	private CoordSys coordSys;
-	private BufferedImage image;
+	private BufferedImage image, imageNode;
 	private Dimension viewSize;
 	private final double panelSizeRatio = 3;
 	private int imageX = 0;
 	private int imageY = 0;
 	private int imageW = 0;
 	private int imageH = 0;
+	ArrayList<Node> nodes = new ArrayList<Node>();
 
 	public MapPanel() {
 		viewSize = new Dimension((int)(screenSize.getWidth()*(2/panelSizeRatio)), (int)screenSize.getHeight());
 		this.setPreferredSize(viewSize);
         
 		fillPanel();
+		
+		//Voor testen: hier handmatig de nodelist vullen
+		Node node = new Node(1, 1000, 1000);
+		nodes.add(node);
 	}
 
 	private void fillPanel() {
@@ -45,12 +51,28 @@ public class MapPanel extends JPanel{
 		this.addMouseListener(mml);
 		this.addMouseMotionListener(mml);
 		this.setBorder(BorderFactory.createMatteBorder(7, 10, 7, 7, new Color(189,133,57)));
+		
+		addNodes();
+	}
+	
+	private void addNodes() {
+		for(Node node : nodes) {
+			try {
+				imageNode = ImageIO.read(new File("src/node.png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2D = (Graphics2D) g;
 		super.paintComponent(g);
 	    g2D.drawImage(image, imageX, imageY, imageW, imageH, this);  
+	    for(Node node : nodes) {
+	    	g2D.drawImage(imageNode, 100, 100, 30, 30, this);
+	    }
+	      
     }
 	
 	public void setImageProperties(int x, int y, int w, int h) {
